3548963f47d8d6bcec0b6e41267c5e53abd837cd
Istvan Marki <isma@bluefragments.com>
2019-10-18 21:55:36 +0200

    initial commit

---
+++ RDJTPService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RDJTPService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
