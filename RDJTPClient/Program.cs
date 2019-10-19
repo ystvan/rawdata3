3e897228507c90528d160c9ae0b70cc64f0b8e47
Istvan Marki <isma@bluefragments.com>
2019-10-19 16:49:48 +0200

    Basic echo client server

---
+++ RDJTPClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace RDJTPClient
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.Title = "RDJTPClient";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            for (int i = 0; i < 5; i++)
+            {
+                var client = new Client();
+                client.Start();
+                Console.WriteLine($"Starting client {i}\n");
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine($"Press any key to terminate...");
+            Console.ReadKey();
+        }
+    }
+}
