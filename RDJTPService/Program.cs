4a1cfeb59ec16336ae1bd7abbaaf634df0d9d5c7
Istvan Marki <isma@bluefragments.com>
2019-10-21 15:02:17 +0200

    20/28 passed

--- RDJTPService/Program.cs
+++ RDJTPService/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RDJTP.Core;
+using System;
+using System.Collections.Generic;
 
 namespace RDJTPService
 {
@@ -10,8 +12,21 @@ namespace RDJTPService
             Console.ForegroundColor = ConsoleColor.Red;
 
             Console.WriteLine($"Server is starting...!\n");
+
             var server = new Server();
             server.Start();
+        }
+
+        public static List<Category> GetCategories()
+        {
+            var categories = new List<Category>
+            {
+                new Category { Id = 1, Name = "Beverages" },
+                new Category { Id = 2, Name = "Condiments" },
+                new Category { Id = 3, Name = "Confections" },
+            };
+
+            return categories;
         }
     }
 }
